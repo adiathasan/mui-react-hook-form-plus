264f1bdf2e9e3fb6e779c6b7b38fa34798220be6
adiathasan <adiathasan.me@gmail.com>
2022-09-18 16:28:40 +0600

    Updated t0 0.0.5

--- rollup.config.js
+++ rollup.config.js
@@ -3,17 +3,19 @@ import postcss from 'rollup-plugin-postcss';
 
 import pkg from './package.json';
 
-export default {
-	input: 'src/index.ts',
-	output: [
-		{
-			file: pkg.main,
-			format: 'cjs',
-			exports: 'named',
-			sourcemap: true,
-			strict: false,
-		},
-	],
-	plugins: [typescript(), postcss({ modules: true })],
-	external: ['react', 'react-dom', 'react-hook-form', '@mui/material/TextField', '@mui/material/Grid'],
-};
+export default [
+	{
+		input: 'src/index.ts',
+		output: [
+			{
+				file: pkg.main,
+				format: 'cjs',
+				exports: 'named',
+				sourcemap: true,
+				strict: false,
+			},
+		],
+		plugins: [typescript({ objectHashIgnoreUnknownHack: true }), postcss({ modules: true })],
+		external: ['react', 'react-dom', 'react-hook-form', '@mui/material/TextField', '@mui/material/Grid'],
+	},
+];
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,2 @@
-export { HookTextField, IFormInputTextProps } from '@components/HookTextField';
-export { UseHookFormRegisterFn, useHookForm } from '@lib/react-hook-form/useHookForm';
+export { HookTextField, IFormInputTextProps } from './components/HookTextField';
+export { UseHookFormRegisterFn, useHookForm } from './lib/react-hook-form/useHookForm';
