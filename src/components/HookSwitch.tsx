fca8a676d9b0068e0b5801415ab94b5a38a4e32e
adiathasan <adiathasan.me@gmail.com>
2022-09-21 19:08:52 +0600

    Added Switch and docs for it

---
+++ src/components/HookSwitch.tsx
@@ -0,0 +1,96 @@
+import { SwitchProps, FormControlLabelProps, GridProps, Grid, FormControlLabel, Switch } from '@mui/material';
+import { callAll } from '@utils/misc';
+import * as React from 'react';
+
+import {
+	Controller,
+	FieldValues,
+	FormState,
+	UseControllerProps,
+	UseFormSetValue,
+	UseFormTrigger,
+} from 'react-hook-form';
+
+export interface IFormSwitchProps<T extends FieldValues = FieldValues> extends UseControllerProps<T> {
+	switchProps?: SwitchProps;
+	labelProps?: Omit<FormControlLabelProps, 'control' | 'label'>;
+	label?: React.ReactNode;
+	formState?: FormState<T>;
+	gridProps?: GridProps;
+	setValue?: UseFormSetValue<T>;
+	trigger?: UseFormTrigger<T>;
+	config?: {
+		/**
+		 */
+	};
+}
+
+// ====================================================
+
+/**
+ *
+ * @description A text input field that uses react-hook-form to manage the form state.
+ * @param {IFormSwitchProps}
+ *
+ * formState: The form state to use from the useHookForm hook that we are using.
+ *
+ * @returns {React.ReactElement}
+ *
+ */
+
+// ====================================================
+const HookSwitch = <T extends FieldValues>({ gridProps, ...props }: IFormSwitchProps<T>): React.ReactElement => {
+	/**
+	 * if grid props are passed in we wrap the text field in a grid
+	 */
+	if (gridProps) {
+		return (
+			<Grid item {...gridProps}>
+				<Component {...props} />
+			</Grid>
+		);
+	}
+
+	return <Component {...props} />;
+};
+
+/**
+ *
+ * @description The actual component that is returned from the HookSwitch component
+ *
+ */
+const Component = <T extends FieldValues>({
+	switchProps = {},
+	setValue,
+	labelProps,
+	trigger,
+	config = {},
+	gridProps,
+	label,
+	...restC
+}: IFormSwitchProps<T>) => {
+	const { onChange, ...rest } = switchProps;
+
+	/**
+	 * we don't want to pass onChange to the Switch
+	 * we want to use the hook-form onChange
+	 * But we also want to keep the onChange passed in the switchProps
+	 * So we use callAll to merge the two
+	 */
+	const onChangeRef = React.useRef(onChange);
+
+	return (
+		<Controller
+			{...restC}
+			render={({ field: { onChange: onChangeI, value = false } }) => (
+				<FormControlLabel
+					{...labelProps}
+					label={label}
+					control={<Switch checked={value} {...rest} onChange={callAll(onChangeI, onChangeRef.current)} />}
+				/>
+			)}
+		/>
+	);
+};
+
+export default HookSwitch;
---
+++ src/stories/HookSwitch.stories.tsx
@@ -0,0 +1,72 @@
+import * as React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import HookSwitch from '@components/HookSwitch';
+import { Grid } from '@mui/material';
+import { useWatch } from 'react-hook-form';
+import { useHookForm } from '@lib/react-hook-form/useHookForm';
+
+const stories = storiesOf('HookSwitch', module);
+
+stories.add('@HookSwitch', () => {
+	// import {  Grid } from '@mui/material';
+	// import { useWatch } from 'react-hook-form';
+	// import { HookSwitch, useHookForm } from 'mui-react-hook-form-plus';
+
+	const defaultValues = {
+		person: {
+			isActive: false,
+			isAgent: true,
+		},
+	};
+
+	const { registerState, control } = useHookForm({
+		defaultValues,
+	});
+
+	const isAdmin = useWatch({
+		control,
+		name: 'person.isActive',
+	});
+
+	return (
+		<div>
+			<h3>This is a Switch hooked up with react-hook-form</h3>
+			<h4>
+				Supports `labelProps` & `switchProps` from{' '}
+				<a href='https://mui.com/material-ui/react-switch/' target='_blank' rel='noopener noreferrer'>
+					MUI
+				</a>{' '}
+			</h4>
+			<Grid container columnSpacing={{ xs: 1, md: 3 }}>
+				<HookSwitch
+					{...registerState('person.isActive')}
+					label='Active'
+					gridProps={{
+						xs: 4,
+					}}
+				/>
+				<Grid item alignSelf='center' xs={4}>
+					I'm {isAdmin ? 'Admin' : 'Not Admin'}
+				</Grid>
+				<HookSwitch
+					{...registerState('person.isAgent')}
+					label='Agent'
+					switchProps={{
+						onChange(_event, checked) {
+							setTimeout(() => {
+								alert(`I'm ${checked ? 'an agent' : 'not an agent'}`);
+							}, 300);
+						},
+					}}
+					labelProps={{
+						labelPlacement: 'start',
+					}}
+					gridProps={{
+						xs: 4,
+					}}
+				/>
+			</Grid>
+		</div>
+	);
+});
