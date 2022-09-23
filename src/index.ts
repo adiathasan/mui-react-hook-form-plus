39e778e4f678769ca66c758fc0a431b442b83342
adiathasan <adiathasan.me@gmail.com>
2022-09-23 07:20:40 +0600

    push - storybook - master - deploy - vercel

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export { useFillFormValues } from './lib/react-hook-form/useFillFormValues';
 export { UseHookFormRegisterFn, useHookForm } from './lib/react-hook-form/useHookForm';
 
 export { HookRating, HookRatingProps } from './components/HookRating';
+export { HookSlider, HookSliderProps } from './components/HookSlider';
 export { HookCheckBoxProps, HookCheckBox } from './components/HookCheckBox';
 export { HookTextField, HookTextFieldProps } from './components/HookTextField';
 export { HookRadioButton, HookRadioButtonProps } from './components/HookRadioButton';
